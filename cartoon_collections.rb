c8e0846ff5c221dfa8721d75ff39f49d16d6eb1b
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-26 12:59:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? > 4 
+  planeteer_calls.length.any? > 4 
 end
 
 def find_the_cheese# code an argument here
